4bc4316ab088bb7176a1a84475e12a7964f8e71b
Alekhya Sai <alekhyasaip@gmail.com>
2021-10-25 23:24:54 -0700

    Updated Python code

--- Weather-api.py
+++ Weather-api.py
@@ -1,9 +1,12 @@
 import http.client
 
 conn = http.client.HTTPSConnection("api.weatherapi.com")
-payload = ''
-headers = {}
-conn.request("GET", "/v1/current.json?q=Paris&Key=b21348a9184e494aa0934948212610", payload, headers)
+headers = {
+    'x-rapidapi-host': "weatherapi-com.p.rapidapi.com",
+    'x-rapidapi-key': "SIGN-UP-FOR-KEY"
+    }
+
+conn.request("GET", "/ip.json?q=%3CREQUIRED%3E", headers=headers)
 res = conn.getresponse()
 data = res.read()
 print(data.decode("utf-8"))
