5686e12ce6752c6f6ddd947d0fdbc7ce2da435d0
Alekhya Sai <alekhyasaip@gmail.com>
2021-10-25 23:53:59 -0700

    Updating the code

--- Weather-api.py
+++ Weather-api.py
@@ -1,10 +1,15 @@
+#Using HTTP Client
 import http.client
+import pprint
+import json
 
 conn = http.client.HTTPSConnection("api.weatherapi.com")
 payload = ''
 headers = {}
-
-conn.request("GET", "/v1/current.json?q=Paris&Key={{YOURKEY}}", payload, headers)
+conn.request("GET",
+             "/v1/current.json?q=Paris&Key=b21348a9184e494aa0934948212610",
+             payload, headers)
 response = conn.getresponse()
 data = response.read()
-print(data.decode("utf-8"))
+pretty_json = json.loads(data)
+pprint.pprint(pretty_json)
