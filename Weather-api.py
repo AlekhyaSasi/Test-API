da783221d2c5b2868b3729b0e5623b4e34224d2e
Alekhya Sai <alekhyasaip@gmail.com>
2021-10-26 00:25:18 -0700

    Updates to code

--- Weather-api.py
+++ Weather-api.py
@@ -7,9 +7,32 @@ conn = http.client.HTTPSConnection("api.weatherapi.com")
 payload = ''
 headers = {}
 conn.request("GET",
-             "/v1/current.json?q=Paris&Key=b21348a9184e494aa0934948212610",
+             "/v1/current.json?q=Paris&Key={{api_key}}",
              payload, headers)
 response = conn.getresponse()
 data = response.read()
 pretty_json = json.loads(data)
 pprint.pprint(pretty_json)
+
+# Using request library
+
+import requests
+from pprint import pprint
+params = {'q' : 'Paris',
+'key' : 'api_key'}
+url = "http://api.weatherapi.com/v1/current.json"
+
+try:
+  response = requests.get(url, params =params, timeout=5)
+  response.raise_for_status()
+  pprint (response.json())
+    # Code here will only run if the request is successful
+except requests.exceptions.HTTPError as errh:
+    print(errh)
+except requests.exceptions.ConnectionError as errc:
+    print(errc)
+except requests.exceptions.Timeout as errt:
+    print(errt)
+except requests.exceptions.RequestException as err:
+    print(err)
+
